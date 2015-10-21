c3647fc70542ff271972af1c86bf87ed7953d2b3
cuittzq <455164259@qq.com>
2015-10-21 14:34:38 +0000

    加入数据库

---
+++ DBHeper.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+__author__ = 'tzq'
+import MySQLdb
+
+try:
+    conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='test',port=3306)
+    cur=conn.cursor()
+    cur.execute('select * from user')
+    cur.close()
+    conn.close()
+except MySQLdb.Error,e:
+     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
